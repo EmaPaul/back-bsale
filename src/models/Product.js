358a661eafae1f024137af272a5af86ab6430b86
EmaPaul <enmanuelpaulcarrillo.rakata900@gmail.com>
2022-10-15 00:45:23 -0500

    ajustes_prueba

--- src/models/Product.js
+++ src/models/Product.js
@@ -12,36 +12,78 @@ const Productos = class {
       let sql = `SELECT * FROM product WHERE id = ?`;
       return database.execute(sql, [id]);
     }
-  
+
     static findByFilters(name, category, pricemin, pricemax, discountmin, discountmax) {
       let sql = "SELECT * FROM product WHERE 1=1";
-      let queries = [];
-      if (name) {
-        sql += ` AND name LIKE ?`;
-        queries.push(`%${name}%`);
+      let consultas = [];
+      let parametro="Nombre";
+
+      switch(parametro) {
+        case "Nombre":
+          sql += ` AND name LIKE ?`;
+          consultas.push(`%${name}%`);
+          break;
+        
+        case "Categoria":
+          sql += ` AND category LIKE ?`;
+          consultas.push(`%${category}%`);
+          break;
+
+        case "PrecioMin":
+          sql += ` AND price >= ?`;
+          consultas.push(pricemin);
+          break;
+        
+        case "PrecioMax":
+          sql += ` AND price <= ?`;
+          consultas.push(pricemax);
+          break;
+        
+        case "DescuentoMin":
+          sql += ` AND discount >= ?`;
+          consultas.push(discountmin);
+          break;
+        
+        case "DescuentoMax":
+          sql += ` AND discount <= ?`;
+          consultas.push(discountmax);
+          break;
+        
+        default:
+          return parametro;
       }
-      if (category) {
-        sql += ` AND category LIKE ?`;
-        queries.push(`%${category}%`);
-      }
-      if (pricemin) {
-        sql += ` AND price >= ?`;
-        queries.push(pricemin);
-      }
-      if (pricemax) {
-        sql += ` AND price <= ?`;
-        queries.push(pricemax);
-      }
-      if (discountmin) {
-        sql += ` AND discount >= ?`;
-        queries.push(discountmin);
-      }
-      if (discountmax) {
-        sql += ` AND discount <= ?`;
-        queries.push(discountmax);
-      }
-      return database.execute(sql, queries);
+      return database.execute(sql, consultas);
     }
+  
+    // static findByFilters(name, category, pricemin, pricemax, discountmin, discountmax) {
+    //   let sql = "SELECT * FROM product WHERE 1=1";
+    //   let queries = [];
+    //   if (name) {
+    //     sql += ` AND name LIKE ?`;
+    //     queries.push(`%${name}%`);
+    //   }
+    //   if (category) {
+    //     sql += ` AND category LIKE ?`;
+    //     queries.push(`%${category}%`);
+    //   }
+    //   if (pricemin) {
+    //     sql += ` AND price >= ?`;
+    //     queries.push(pricemin);
+    //   }
+    //   if (pricemax) {
+    //     sql += ` AND price <= ?`;
+    //     queries.push(pricemax);
+    //   }
+    //   if (discountmin) {
+    //     sql += ` AND discount >= ?`;
+    //     queries.push(discountmin);
+    //   }
+    //   if (discountmax) {
+    //     sql += ` AND discount <= ?`;
+    //     queries.push(discountmax);
+    //   }
+    //   return database.execute(sql, queries);
+    // }
 }
   
 module.exports = Productos;
